6c9d4d17907d5068d5c7d6e663412cee8add524d
Orthanc173 <paul.sturm@lernende.bildxzug.ch>
2026-08-19 16:37:34 +0200

    DoubleChristiansPipes spawn

--- Games/FlappyLogic.js
+++ Games/FlappyLogic.js
@@ -1,7 +1,4 @@
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
+/*
 const christianPipe = document.querySelector("#ChristianPipe");
 christianPipe.style.position = "absolute";
 christianPipe.style.left = "50%";
@@ -15,3 +12,37 @@ async function moveChristianPipe() {
 }
 
 moveChristianPipe();
+*/
+
+function spawnChristiansPipes() {
+  const LowestChristian = 80;
+  const HighestChristian = 45;
+
+  const DoubleChristHight =
+    Math.floor(Math.random() * (LowestChristian - HighestChristian + 1)) +
+    HighestChristian;
+
+  const ChristianTop = document.createElement("img");
+  ChristianTop.src = "./../GameAssests/FlappyBird/christi.png";
+  ChristianTop.style.position = "absolute";
+  ChristianTop.style.left = "75vw";
+  ChristianTop.style.top = DoubleChristHight - 75 + "vh";
+  ChristianTop.id = "ChristianTopPipe";
+  ChristianTop.style.rotate = "180deg";
+  ChristianTop.style.transform = "scaleX(-1)";
+  let ChristianTopElement = document.body.appendChild(ChristianTop);
+
+  const ChristianBot = document.createElement("img");
+  ChristianBot.src = "./../GameAssests/FlappyBird/christi.png";
+  ChristianBot.style.position = "absolute";
+  ChristianBot.style.left = "75vw";
+  ChristianBot.style.top = DoubleChristHight + "vh";
+  ChristianBot.id = "ChristianBottomPipe";
+  let ChristianBotElement = document.body.appendChild(ChristianBot);
+
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  spawnChristiansPipes();
+});
