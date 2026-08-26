76046e9ce8c08487b0ad98922bba6f4d34b72b71
David <GGrktEZ@users.noreply.github.com>
2026-08-26 11:39:28 +0200

    Made background static

--- Games/FlappyLogic.js
+++ Games/FlappyLogic.js
@@ -1,19 +1,3 @@
-/*
-const christianPipe = document.querySelector("#ChristianPipe");
-christianPipe.style.position = "absolute";
-christianPipe.style.left = "50%";
-christianPipe.style.top = "10%";
-
-async function moveChristianPipe() {
-  while (true) {
-    await sleep(10);
-    christianPipe.style.left = christianPipe.offsetLeft - 2 + "px";
-  } 
-}
-
-moveChristianPipe();
-*/
-
 /**
  * Slleps for a given amount of milisecs.
  *
