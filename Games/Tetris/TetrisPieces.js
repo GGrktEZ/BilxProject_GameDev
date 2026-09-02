6de61f8c3c1019f97cfe28100be38ca90639cdef
David <GGrktEZ@users.noreply.github.com>
2026-09-02 13:49:23 +0200

    arrays4pieces

--- Games/Tetris/TetrisPieces.js
+++ Games/Tetris/TetrisPieces.js
@@ -15,3 +15,200 @@
 // The type number is also the color number on the board. That is why 0 stays
 // free: on the board 0 means "empty cell".
 // ---------------------------------------------------------------------------
+
+const PIECE_I = 1;
+const PIECE_O = 2;
+const PIECE_T = 3;
+const PIECE_S = 4;
+const PIECE_Z = 5;
+const PIECE_J = 6;
+const PIECE_L = 7;
+
+
+/** All piece types, so we can pick a random one. */
+const PIECE_TYPES = [PIECE_I, PIECE_O, PIECE_T, PIECE_S, PIECE_Z, PIECE_J, PIECE_L];
+
+/** One color per piece type. Index 0 is never drawn, it stands for "empty". */
+const PIECE_COLORS = [
+  "#000000", // 0 - empty, unused
+  "#38bdf8", // 1 - I, light blue
+  "#facc15", // 2 - O, yellow
+  "#c084fc", // 3 - T, purple
+  "#4ade80", // 4 - S, green
+  "#f87171", // 5 - Z, red
+  "#60a5fa", // 6 - J, blue
+  "#fb923c", // 7 - L, orange
+];
+
+/**
+ * The shapes. PIECES[type][rotation] gives one matrix.
+ *
+ * Rotation goes 0 -> 1 -> 2 -> 3 -> 0, always clockwise.
+ */
+const PIECES = {};
+
+PIECES[PIECE_I] = [
+  // rotation 0
+  [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  // rotation 1
+  [
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+  ],
+  // rotation 2
+  [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+  ],
+  // rotation 3
+  [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+];
+
+PIECES[PIECE_O] = [
+  // the square looks the same in every rotation
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+  [
+    [1, 1],
+    [1, 1],
+  ],
+];
+
+PIECES[PIECE_T] = [
+  [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 1],
+    [0, 1, 0],
+  ],
+  [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 1, 0],
+  ],
+];
+
+PIECES[PIECE_S] = [
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 1],
+    [0, 0, 1],
+  ],
+  [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  [
+    [1, 0, 0],
+    [1, 1, 0],
+    [0, 1, 0],
+  ],
+];
+
+PIECES[PIECE_Z] = [
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [0, 0, 1],
+    [0, 1, 1],
+    [0, 1, 0],
+  ],
+  [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [0, 1, 0],
+    [1, 1, 0],
+    [1, 0, 0],
+  ],
+];
+
+PIECES[PIECE_J] = [
+  [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 1],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+  [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 0, 1],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+];
+
+PIECES[PIECE_L] = [
+  [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0],
+  ],
+  [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+  ],
+  [
+    [0, 0, 0],
+    [1, 1, 1],
+    [1, 0, 0],
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+  ],
+];
